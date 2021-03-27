62c753614de81bdb873169c447cedd340e589517
Shahab Dad khan <shahabkhan681@gmail.com>
2021-03-27 18:52:59 +0530

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,97 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about d-flex align-center flex-column">
+    <v-container class="about-card my-5">
+      <div class="do d-flex ">
+        <div class="D"></div>
+        <div class="O d-flex align-center justify-center">
+          <div class="check "></div>
+          <!-- <v-icon size="200" color="white">mdi-check</v-icon> -->
+        </div>
+      </div>
+    </v-container>
+    <!-- <v-container tag="span" white--text text-center
+      >This is the most TO-DO app ui kit you've ever seen. yours,for
+      free</v-container
+    > -->
+    <h1 class="white--text font-weight-light my-5">
+      This is the most <span class="grey">TO-DO</span> app ui kit you've ever
+      seen. yours,for free
+    </h1>
+    <h1 class="white--text font-weight-light my-5">
+      Designed to meet your needs
+    </h1>
+    <v-btn elevation="4" class="btn  my-4 ">Start</v-btn>
+    <!-- <span 
+      ></span
+    > -->
   </div>
 </template>
+
+<script>
+export default {
+  //
+};
+</script>
+
+<style scope>
+.about {
+  background-image: url(../img/1.jpg);
+  background-size: cover;
+  height: 100vh;
+  color: gray;
+}
+.about-card {
+  /* background-image: url(../img/2.jpg); */
+  /* background-size: cover; */
+  /* background-color: aqua; */
+  width: 33% !important;
+  height: 41%;
+}
+
+.do {
+  width: 100%;
+  height: 100%;
+  justify-content: space-between;
+}
+
+.D {
+  width: 200px;
+  height: 200px;
+  border: 2px solid white;
+  border-top-right-radius: 50%;
+  border-bottom-right-radius: 50%;
+
+  /* background-color: #fff; */
+}
+
+.O {
+  width: 200px;
+  height: 200px;
+  border: 2px solid whitesmoke;
+  border-radius: 50%;
+  transform: rotate(-40deg);
+}
+
+.check {
+  border-left: 2px solid white;
+  border-bottom: 2px solid white;
+  /* border: 2px solid white; */
+  margin-bottom: 20px;
+  margin-left: 22px;
+  width: 95px;
+  height: 35px;
+}
+
+.grey {
+  transform: rotate(-3deg);
+  /* transform: rotate(-10deg);
+  transition: all 0.2s; */
+  display: inline-block;
+}
+
+.btn:hover {
+  transform: scale(1.1);
+  transition: all 0.2s ease-in;
+  background-color: grey !important;
+}
+</style>
---
+++ src/views/Sign.vue
@@ -0,0 +1,126 @@
+<template>
+  <v-app>
+    <!-- <v-container class="container d-flex"> -->
+    <div class="container d-flex justify-center align-center flex-column">
+      <div class="O d-flex align-center justify-center mb-5">
+        <div class="check "></div>
+        <!-- <v-icon size="200" color="white">mdi-check</v-icon> -->
+      </div>
+      <v-form ref="form" v-model="valid" class="input d-flex flex-column ">
+        <v-text-field
+          :rules="nameRules"
+          label="Name"
+          dark
+          prepend-icon="person"
+          required
+        ></v-text-field>
+        <v-text-field
+          v-model="email"
+          :rules="emailRules"
+          label="E-mail"
+          dark
+          prepend-icon="lock"
+          required
+        ></v-text-field>
+      </v-form>
+      <div class="btn-f d-flex justify-end">
+        <v-btn href="#" elevation="0" class="transparent grey--text"
+          >Forgot Password</v-btn
+        >
+      </div>
+      <v-btn class="my-5 orangered"> Sign In </v-btn>
+      <div class="d-flex flex-row align-center">
+        <div class="grey--text my-5">Don't have an account?</div>
+        <v-btn
+          href="#"
+          elevation="0"
+          class=" transparent white--text text-capitalize"
+          >Sign Up</v-btn
+        >
+      </div>
+    </div>
+    <!-- </v-container> -->
+  </v-app>
+</template>
+
+//
+<script>
+// export default {
+//   name: "HelloWorld",
+
+//   data: () => ({
+//     //
+//   }),
+// };
+//
+</script>
+
+<script>
+export default {
+  data: () => ({
+    valid: true,
+    name: "",
+    nameRules: [
+      (v) => !!v || "Name is required",
+      (v) => (v && v.length <= 10) || "Name must be less than 10 characters",
+    ],
+    email: "",
+    emailRules: [
+      (v) => !!v || "Password is required",
+      (v) => /.+@.+\..+/.test(v) || "Password must be valid",
+    ],
+    select: null,
+  }),
+
+  methods: {
+    signIn() {},
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  max-width: 100%;
+  height: 100%;
+  /* background-color: violet; */
+  background: url(../img/m1.jpg) center fixed;
+  background-size: cover;
+}
+
+.input {
+  min-width: 35% !important;
+  min-height: 20%;
+  /* color: white;
+  background-color: #fff; */
+}
+
+.O {
+  width: 200px;
+  height: 200px;
+  /* border: 2px solid whitesmoke; */
+  border-radius: 50%;
+  transform: rotate(-40deg);
+  margin-top: -55px;
+  background-color: whitesmoke;
+}
+
+.check {
+  border-left: 20px solid orangered;
+  border-bottom: 20px solid orangered;
+  /* border: 2px solid white; */
+  margin-bottom: 20px;
+  margin-left: 22px;
+  width: 95px;
+  height: 45px;
+}
+
+.btn-f {
+  width: 37%;
+}
+
+.btn-f:hover {
+  transform: scale(1.1);
+  transition: all 0.2s ease-in;
+  background-color: grey !important;
+}
+</style>
